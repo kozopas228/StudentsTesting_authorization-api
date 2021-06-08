0c64d063d97eeb28d3fa8e9dd7f58cd53751cc0f
nik.001140@gmail.com <nik.001140@gmail.com>
2021-06-08 13:09:09 +0300

    Refactoring 2.0

--- Authorization_API.IntegrationTests/UserManagementControllerTests.cs
+++ Authorization_API.IntegrationTests/UserManagementControllerTests.cs
@@ -1,10 +1,8 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Authorization_API.IntegrationTests.Util;
 using Authorization_Models;
-using AutoFixture;
 using Newtonsoft.Json;
 using Xunit;
 
